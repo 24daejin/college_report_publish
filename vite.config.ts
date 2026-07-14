20c08620014bd19ed2f07e709ff32db82142dfb1
24daejin <49755322+24daejin@users.noreply.github.com>
2026-07-14 11:12:35 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/college_report_publish/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
